48769d9d3068629bbaab6d7e6d1f916dbd1d61c5
Ekaterina Tranchand <114387307+catherine-tranchand@users.noreply.github.com>
2022-11-14 14:38:55 +0100

    Add files via upload

---
+++ jour1/Job01/index.php
@@ -0,0 +1,18 @@
+<?php 
+$str="LaPlateforme";
+$str2="Vive";
+$str3="!";
+echo "$str2 $str $str3";
+
+
+$val=6;
+echo "$val" + 4;
+
+
+$myBool=true;
+echo "$myBool";
+
+$myBool=false;
+echo "$myBool";
+
+?>
