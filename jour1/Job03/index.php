94ddea02db09d35e590c06f4a69a08d6948e7893
Ekaterina Tranchand <114387307+catherine-tranchand@users.noreply.github.com>
2022-11-14 16:38:21 +0100

    Add files via upload

---
+++ jour1/Job03/index.php
@@ -0,0 +1,37 @@
+<?php
+$str="Chaines de characteres";
+$booleans=true;
+$int=56;
+$float=11.5;
+$x="String";
+$y="Booleans";
+$a="Primitif";
+$b="Integer";
+$c="Float";
+
+
+echo "
+<table>
+    <tbody>
+        <tr>
+            <th>Type</th>
+            <th>Nom</th>
+            <th>Valeur</th>
+        </tr>
+
+        <tr><td>$a</td><td>$x</td><td>$str</td></tr>
+        <tr><td>$a</td><td>$y</td><td>$booleans</td></tr>
+        <tr><td>$a </td><td>$c</td><td>$float</td></tr>
+        <tr><td>$a</td><td>$b</td><td>$int</td></tr>
+    </tbody>
+</table>
+";
+
+
+ 
+ 
+?>
+ 
+
+
+
