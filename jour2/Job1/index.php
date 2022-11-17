76c014a77e36be1ac82131cb2cfa8b81c4973881
Ekaterina Tranchand <114387307+catherine-tranchand@users.noreply.github.com>
2022-11-17 18:56:20 +0100

    Add files via upload

--- jour2/Job1/index.php
+++ jour2/Job1/index.php
@@ -1,8 +1,7 @@
-
 <?php
 
 for ($a=0; $a<=1337; $a++){   //For the range of numbers between 0 and 1337
-    if ($a == 42){.           // In this case when number is equal to 42, display "42" bold and underline it
+    if ($a == 42){        // In this case when number is equal to 42, display "42" bold and underline it
         echo '
         <b><u>' .$a. '</u></b>
         <br>';               
